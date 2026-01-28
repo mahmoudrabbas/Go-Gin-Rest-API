87de5b5b8bdcd60dec5d98c64bbc66d2f8299d8d
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-01-29 00:38:12 +0200

    setup'

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	
+
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `<h1>Hello World 5</h1>`);
+	} )
+
+
+	http.ListenAndServe(":9090", nil);
+}
